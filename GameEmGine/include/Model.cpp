d9693d56fdba2b7e5a6e0430880792f0fa50b41a
Raw-Bert <robert.andersen@uoit.net>
2018-11-09 14:54:47 -0500

    Just made it brighter

--- GameEmGine/include/Model.cpp
+++ GameEmGine/include/Model.cpp
@@ -33,7 +33,7 @@ void Model::render(GLSLCompiler& shader, Camera& cam)
 
 	glUniform4fv(shader.getUniformLocation("LightPosition"), 1,position);
 
-	glUniform3f(shader.getUniformLocation("LightAmbient"), 0.5f, 0.5f, 0.5f);
+	glUniform3f(shader.getUniformLocation("LightAmbient"), 0.7f, 0.7f, 0.7f);
 	glUniform3f(shader.getUniformLocation("LightDiffuse"), 0.0f, 0.0f, 1.0f);
 	glUniform3f(shader.getUniformLocation("LightSpecular"), 0.8f, 0.2f, 0.2f);
 
