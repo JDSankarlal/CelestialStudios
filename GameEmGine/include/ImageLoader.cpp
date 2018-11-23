885573ba5a746d49781c9dba463f9e5b1107c175
Emory Wynn <emory.wynn@uoit.net>
2018-11-23 14:44:31 -0500

    Stage Before Merge

--- GameEmGine/include/ImageLoader.cpp
+++ GameEmGine/include/ImageLoader.cpp
@@ -8,7 +8,7 @@ Texture2D ImageLoader::loadImage2D(const char * path)
 
 	if (image == nullptr)
 	{
-		printf("Image returned with null pointer\n");
+		printf("Image \"%s\", returned with null pointer\n",path);
 		return texture;
 	}
 
@@ -31,31 +31,31 @@ Texture2D ImageLoader::loadImage2D(const char * path)
 	return texture;
 }
 
-Texture3D ImageLoader::loadImage3D(const char * path)
-{
-	Texture3D texture;
-	int width=0, height=0,depth=0;
-	unsigned char *image=nullptr;
-
-	//Load 3D texture information
-
-
-	//Bind texture to model
-	glGenTextures(1, &texture.id);
-	glBindTexture(GL_TEXTURE_3D, texture.id);
-	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA, width, height,depth, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glGenerateMipmap(GL_TEXTURE_3D);
-	SOIL_free_image_data(image);
-	glBindTexture(GL_TEXTURE_3D, 0);
-
-	texture.width = width;
-	texture.height = height;
-	texture.depth = depth;
-
-	return texture;
-}
+//Texture3D ImageLoader::loadImage3D(const char * path)
+//{
+//	Texture3D texture;
+//	int width=0, height=0,depth=0;
+//	unsigned char *image=nullptr;
+//
+//	//Load 3D texture information
+//
+//
+//	//Bind texture to model
+//	glGenTextures(1, &texture.id);
+//	glBindTexture(GL_TEXTURE_3D, texture.id);
+//	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA, width, height,depth, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+//	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+//	glGenerateMipmap(GL_TEXTURE_3D);
+//	SOIL_free_image_data(image);
+//	glBindTexture(GL_TEXTURE_3D, 0);
+//
+//	texture.width = width;
+//	texture.height = height;
+//	texture.depth = depth;
+//
+//	return texture;
+//}
