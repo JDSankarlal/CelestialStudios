64db85bf7ce532ffc04248cb9f1137dd0b189b7f
Emory Wynn <emory.wynn@uoit.net>
2019-03-30 12:31:50 -0400

    tried to un-fuck the wrong repo but it's fine

--- GameEmGine/include/fileIO.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdio>
-class fileIO
-{
-public:
-	//No longer needed but good resource for how to read in image files in C binary
-	static int readFileToBuffer(const char* path,unsigned char * buffer);
-};
-
