919841f971f6788f215fc29aaa1696d080f47b94
Robert <robert.andersen@uoit.net>
2019-04-02 05:39:03 -0400

    fs

--- First Game/main.cpp
+++ First Game/main.cpp
@@ -9,7 +9,7 @@ int main()
 
 	GameEmGine::init("The Real Game", 1920, 1080, 0, 0, 0, false);
 	GameEmGine::setScene(new Splash);
-	//GameEmGine::getWindow()->setFullScreen(true);
+	GameEmGine::getWindow()->setFullScreen(true);
 	GameEmGine::run();
 	return 0;
 }
